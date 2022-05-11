49e8fbbd2a66eb904c5bbbb0b28f5106f3041540
julianpetrini <bwmisojos@gmail.com>
2022-05-11 09:47:11 +0200

    update_alert

---
+++ main.js
@@ -0,0 +1,28 @@
+//FUNCTION VALIDATION 
+function validation() {
+
+    sessionStorage.setItem("firstName", document.getElementById("fname").value);
+    sessionStorage.setItem("email", document.getElementById("email").value);
+
+    let firstName = document.getElementById("fname").value
+    let email = document.getElementById("email").value
+
+    if (firstName == "" || email == "") {
+        alert("Por favor complete los datos");
+
+        return false
+    }
+    else {
+        alert("Muchas gracias, usted sera re dirijido");
+
+    }
+};
+
+function newPage() {
+
+    let firstname = sessionStorage.getItem("firstName");
+    let email = sessionStorage.getItem("email");
+
+    document.getElementById("result").innerHTML = firstname + " gracias por registrarte en nuestra base de datos, pronto te enviaremos a " + email + " tu descuento y muchas mas novedades."
+
+}
